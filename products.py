4a79cbd684030619b8cd6ad461545b4fe85c44b0
Agustin Gomez <agusleongomez@gmail.com>
2023-01-11 23:46:12 -0300

    Big Bang

---
+++ products.py
@@ -0,0 +1,6 @@
+products = [
+  {"name": "laptop", "price": 500, "quantity": 6},
+  {"name": "mouse", "price": 40, "quantity": 10},
+  {"name": "monitor", "price": 150, "quantity": 8},
+  {"name": "teclado", "price": 60, "quantity": 12},
+]
